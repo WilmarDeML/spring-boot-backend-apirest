5ba7a3d75c757687f1ad9ff3f759762e65f9237e
Wilmar Zapata <wavilla@stefanini.com>
2022-07-30 09:09:58 -0500

    Insertar valores de prueba en la base de datos

---
+++ src/main/resources/import.sql
@@ -0,0 +1,12 @@
+/* Populate tabla clientes */
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Wilmar', 'Zapata', 'wilmar@wmail.wom', '2022-05-21');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Migue', 'Zapata', 'migue@wmail.wom', '2022-06-25');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Isaac', 'Zapata', 'isaac@wmail.wom', '2022-06-11');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Helen', 'Villa', 'helen@wmail.wom', '2022-06-05');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Antonio', 'Zapata', 'antonio@wmail.wom', '2022-06-17');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Manuela', 'Zapata', 'manuela@wmail.wom', '2022-07-13');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Luciana', 'Tobón', 'luciana@wmail.wom', '2022-07-08');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Sandra', 'Gil', 'sandra@wmail.wom', '2022-07-22');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Liliana', 'Londoño', 'liliana@wmail.wom', '2022-04-25');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Michael', 'Zapata', 'michael@wmail.wom', '2022-04-10');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Camila', 'Bohórquez', 'camila@wmail.wom', '2022-06-05');
