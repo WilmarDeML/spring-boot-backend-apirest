5e31e831f4e6da5b4bf72360e0d1d880d8de3b2b
Wilmar Zapata <wavilla@stefanini.com>
2022-08-01 07:07:48 -0500

    Cambiar a tipo de ReponseEntity el método listById

--- src/main/java/com/wilmardeml/springboot/backend/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/wilmardeml/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.wilmardeml.springboot.backend.apirest.models.entity.Cliente;
@@ -23,7 +24,10 @@ public class ClienteRestController {
 	}
 
 	@GetMapping("clientes/{id}")
-	public Cliente listById(@PathVariable Long id) { return  clienteService.findById(id); }
+	public ResponseEntity<?> listById(@PathVariable Long id) {
+		Cliente cliente = clienteService.findById(id);
+		return new ResponseEntity<Cliente>(cliente, HttpStatus.OK);
+	}
 
 	@PostMapping("clientes")
 	@ResponseStatus(HttpStatus.CREATED)
