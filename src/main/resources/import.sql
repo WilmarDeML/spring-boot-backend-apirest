4a9a6939575bd8a4fc2d1428cd9a0de3ab23e4a2
Wilmar Zapata <wavilla@stefanini.com>
2022-08-11 00:26:43 -0500

    Insertar regiones de prueba a la tabla

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,12 +1,22 @@
+/* Populate tabla regiones */
+INSERT INTO regiones (id, nombre) VALUES (1, 'Sudamérica')
+INSERT INTO regiones (id, nombre) VALUES (2, 'Centroamérica')
+INSERT INTO regiones (id, nombre) VALUES (3, 'Norteamérica')
+INSERT INTO regiones (id, nombre) VALUES (4, 'Europa')
+INSERT INTO regiones (id, nombre) VALUES (5, 'Asia')
+INSERT INTO regiones (id, nombre) VALUES (6, 'Africa')
+INSERT INTO regiones (id, nombre) VALUES (7, 'Oceanía')
+INSERT INTO regiones (id, nombre) VALUES (8, 'Antártida')
+
 /* Populate tabla clientes */
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Wilmar', 'Zapata', 'wilmar@wmail.wom', '2022-05-21');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Migue', 'Zapata', 'migue@wmail.wom', '2022-06-25');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Isaac', 'Zapata', 'isaac@wmail.wom', '2022-06-11');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Helen', 'Villa', 'helen@wmail.wom', '2022-06-05');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Antonio', 'Zapata', 'antonio@wmail.wom', '2022-06-17');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Manuela', 'Zapata', 'manuela@wmail.wom', '2022-07-13');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Luciana', 'Tobón', 'luciana@wmail.wom', '2022-07-08');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Sandra', 'Gil', 'sandra@wmail.wom', '2022-07-22');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Liliana', 'Londoño', 'liliana@wmail.wom', '2022-04-25');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Michael', 'Zapata', 'michael@wmail.wom', '2022-04-10');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Camila', 'Bohórquez', 'camila@wmail.wom', '2022-06-05');
+INSERT INTO clientes (nombre, apellido, email, create_at, region_id) VALUES('Wilmar', 'Zapata', 'wilmar@wmail.wom', '2022-05-21', 1);
+INSERT INTO clientes (nombre, apellido, email, create_at, region_id) VALUES('Migue', 'Zapata', 'migue@wmail.wom', '2022-06-25', 2);
+INSERT INTO clientes (nombre, apellido, email, create_at, region_id) VALUES('Isaac', 'Zapata', 'isaac@wmail.wom', '2022-06-11', 4);
+INSERT INTO clientes (nombre, apellido, email, create_at, region_id) VALUES('Helen', 'Villa', 'helen@wmail.wom', '2022-06-05', 4);
+INSERT INTO clientes (nombre, apellido, email, create_at, region_id) VALUES('Antonio', 'Zapata', 'antonio@wmail.wom', '2022-06-17', 3);
+INSERT INTO clientes (nombre, apellido, email, create_at, region_id) VALUES('Manuela', 'Zapata', 'manuela@wmail.wom', '2022-07-13', 3);
+INSERT INTO clientes (nombre, apellido, email, create_at, region_id) VALUES('Luciana', 'Tobón', 'luciana@wmail.wom', '2022-07-08', 5);
+INSERT INTO clientes (nombre, apellido, email, create_at, region_id) VALUES('Sandra', 'Gil', 'sandra@wmail.wom', '2022-07-22', 6);
+INSERT INTO clientes (nombre, apellido, email, create_at, region_id) VALUES('Liliana', 'Londoño', 'liliana@wmail.wom', '2022-04-25', 8);
+INSERT INTO clientes (nombre, apellido, email, create_at, region_id) VALUES('Michael', 'Zapata', 'michael@wmail.wom', '2022-04-10', 1);
+INSERT INTO clientes (nombre, apellido, email, create_at, region_id) VALUES('Camila', 'Bohórquez', 'camila@wmail.wom', '2022-06-05', 7);
