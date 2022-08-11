f2e8ddcb882322617eb23ca0ee7f841bfdbdaeb4
Wilmar Zapata <wavilla@stefanini.com>
2022-08-11 00:21:22 -0500

    Implementar método con la notación Query para obtener todas las regiones

--- src/main/java/com/wilmardeml/springboot/backend/apirest/models/dao/IClienteDao.java
+++ src/main/java/com/wilmardeml/springboot/backend/apirest/models/dao/IClienteDao.java
@@ -1,10 +1,15 @@
 package com.wilmardeml.springboot.backend.apirest.models.dao;
 
+import com.wilmardeml.springboot.backend.apirest.models.entity.Region;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 
 import com.wilmardeml.springboot.backend.apirest.models.entity.Cliente;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface IClienteDao extends JpaRepository<Cliente, Long> {
 
+    @Query("from Region")
+    List<Region> findAllRegiones();
 }
