6863d8b9b75c7e06ed114991fe5c44ebcc72fe15
Wilmar Zapata <wavilla@stefanini.com>
2022-08-23 14:09:23 -0500

    Creando tabla ItemFactura y Producto con sus respectivas relaciones

---
+++ src/main/java/com/wilmardeml/springboot/backend/apirest/models/entity/ItemFactura.java
@@ -0,0 +1,49 @@
+package com.wilmardeml.springboot.backend.apirest.models.entity;
+
+import javax.persistence.*;
+import java.io.Serial;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "facturas_items")
+public class ItemFactura implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer cantidad;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Producto producto;
+
+    public Double getImporte() {
+        return cantidad.doubleValue() * producto.getPrecio();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+}
