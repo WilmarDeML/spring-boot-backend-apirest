2d2a8bd8803d188f7bc990b6ab547267ed1c06bd
Wilmar Zapata <wavilla@stefanini.com>
2022-09-17 18:45:20 -0500

    Implementando método para crear factura y Refactorizando path

--- src/main/java/com/wilmardeml/springboot/backend/apirest/controllers/FacturaRestController.java
+++ src/main/java/com/wilmardeml/springboot/backend/apirest/controllers/FacturaRestController.java
@@ -11,27 +11,33 @@ import java.util.List;
 
 @CrossOrigin(origins = {"http://localhost:4201"})
 @RestController
-@RequestMapping("api")
+@RequestMapping("api/facturas")
 public class FacturaRestController {
 
     @Autowired
     private IClienteService clienteService;
 
-    @GetMapping("facturas/{id}")
+    @GetMapping("{id}")
     @ResponseStatus(code = HttpStatus.OK)
     public Factura show(@PathVariable Long id) {
         return clienteService.findFacturaById(id);
     }
 
-    @DeleteMapping("facturas/{id}")
+    @DeleteMapping("{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Long id) {
         clienteService.deleteFacturaById(id);
     }
 
-    @GetMapping("facturas/filtrar-productos/{term}")
+    @GetMapping("filtrar-productos/{term}")
     @ResponseStatus(code = HttpStatus.OK)
     public List<Producto> filtrarProductos(@PathVariable String term) {
         return clienteService.findProductoByNombre(term);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Factura crear(@RequestBody Factura factura) {
+        return clienteService.saveFactura(factura);
     }
 }
