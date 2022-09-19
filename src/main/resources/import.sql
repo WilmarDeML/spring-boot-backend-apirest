c7d0b558206b63c07d7fce64d4d8e40b2ada6505
Wilmar Zapata <wavilla@stefanini.com>
2022-09-19 06:22:22 -0500

    cambiando enconding a ISO-8859-1

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,12 +1,12 @@
 /* Populate tabla regiones */
-INSERT INTO regiones (id, nombre) VALUES (1, 'Sudam√©rica')
-INSERT INTO regiones (id, nombre) VALUES (2, 'Centroam√©rica')
-INSERT INTO regiones (id, nombre) VALUES (3, 'Norteam√©rica')
+INSERT INTO regiones (id, nombre) VALUES (1, 'SudamÈrica')
+INSERT INTO regiones (id, nombre) VALUES (2, 'CentroamÈrica')
+INSERT INTO regiones (id, nombre) VALUES (3, 'NorteamÈrica')
 INSERT INTO regiones (id, nombre) VALUES (4, 'Europa')
 INSERT INTO regiones (id, nombre) VALUES (5, 'Asia')
 INSERT INTO regiones (id, nombre) VALUES (6, 'Africa')
-INSERT INTO regiones (id, nombre) VALUES (7, 'Ocean√≠a')
-INSERT INTO regiones (id, nombre) VALUES (8, 'Ant√°rtida')
+INSERT INTO regiones (id, nombre) VALUES (7, 'OceanÌa')
+INSERT INTO regiones (id, nombre) VALUES (8, 'Ant·rtida')
 
 /* Populate tabla clientes */
 INSERT INTO clientes (nombre, apellido, email, create_at, region_id) VALUES('Wilmar', 'Zapata', 'wilmar@wmail.wom', '2022-05-21', 1);
@@ -15,11 +15,11 @@ INSERT INTO clientes (nombre, apellido, email, create_at, region_id) VALUES('Isa
 INSERT INTO clientes (nombre, apellido, email, create_at, region_id) VALUES('Helen', 'Villa', 'helen@wmail.wom', '2022-06-05', 4);
 INSERT INTO clientes (nombre, apellido, email, create_at, region_id) VALUES('Antonio', 'Zapata', 'antonio@wmail.wom', '2022-06-17', 3);
 INSERT INTO clientes (nombre, apellido, email, create_at, region_id) VALUES('Manuela', 'Zapata', 'manuela@wmail.wom', '2022-07-13', 3);
-INSERT INTO clientes (nombre, apellido, email, create_at, region_id) VALUES('Luciana', 'Tob√≥n', 'luciana@wmail.wom', '2022-07-08', 5);
+INSERT INTO clientes (nombre, apellido, email, create_at, region_id) VALUES('Luciana', 'TobÛn', 'luciana@wmail.wom', '2022-07-08', 5);
 INSERT INTO clientes (nombre, apellido, email, create_at, region_id) VALUES('Sandra', 'Gil', 'sandra@wmail.wom', '2022-07-22', 6);
-INSERT INTO clientes (nombre, apellido, email, create_at, region_id) VALUES('Liliana', 'Londo√±o', 'liliana@wmail.wom', '2022-04-25', 8);
+INSERT INTO clientes (nombre, apellido, email, create_at, region_id) VALUES('Liliana', 'LondoÒo', 'liliana@wmail.wom', '2022-04-25', 8);
 INSERT INTO clientes (nombre, apellido, email, create_at, region_id) VALUES('Michael', 'Zapata', 'michael@wmail.wom', '2022-04-10', 1);
-INSERT INTO clientes (nombre, apellido, email, create_at, region_id) VALUES('Camila', 'Boh√≥rquez', 'camila@wmail.wom', '2022-06-05', 7);
+INSERT INTO clientes (nombre, apellido, email, create_at, region_id) VALUES('Camila', 'BohÛrquez', 'camila@wmail.wom', '2022-06-05', 7);
 
 /* Populate tabla usuarios */
 INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('Wilmar', '$2a$10$pJ69JCQn.Lzk2r4zX9Az5uZFv7Fiu/igJZmpdisuDUEh6RJT1JQC6', 1, 'Wilmar', 'Zapata', 'wilmar@wmail.wom')
@@ -36,12 +36,12 @@ INSERT INTO usuarios_roles (usuario_id, rol_id) VALUES (2, 1)
 
 /* Populate tabla productos */
 INSERT INTO productos (nombre, precio, create_at) VALUES ('Panasonic Pantalla LCD', 259990, NOW());
-INSERT INTO productos (nombre, precio, create_at) VALUES ('Sony C√°mara digital DSC-W320B', 123490, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Sony C·mara digital DSC-W320B', 123490, NOW());
 INSERT INTO productos (nombre, precio, create_at) VALUES ('Apple iPod shuffle', 1499990, NOW());
 INSERT INTO productos (nombre, precio, create_at) VALUES ('Sony Notebook Z110', 37990, NOW());
 INSERT INTO productos (nombre, precio, create_at) VALUES ('Hewlett Packard Multifuncional F2280', 69990, NOW());
 INSERT INTO productos (nombre, precio, create_at) VALUES ('Bianchi Bicicleta Aro 26', 69990, NOW());
-INSERT INTO productos (nombre, precio, create_at) VALUES ('Mica C√≥moda 5 cajones', 299990, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Mica CÛmoda 5 cajones', 299990, NOW());
 
 /* Populate tabla facturas */
 INSERT INTO facturas (descripcion, observacion, cliente_id, create_at) VALUES ('Factura equipos de oficina', null, 1, NOW());
@@ -50,5 +50,5 @@ INSERT INTO facturas_items (cantidad, factura_id, producto_id) VALUES (2, 1, 4);
 INSERT INTO facturas_items (cantidad, factura_id, producto_id) VALUES (1, 1, 5);
 INSERT INTO facturas_items (cantidad, factura_id, producto_id) VALUES (1, 1, 7);
 
-INSERT INTO facturas (descripcion, observacion, cliente_id, create_at) VALUES ('Factura Bicicleta', 'Edici√≥n limitada!', 1, NOW());
+INSERT INTO facturas (descripcion, observacion, cliente_id, create_at) VALUES ('Factura Bicicleta', 'EdiciÛn limitada!', 1, NOW());
 INSERT INTO facturas_items (cantidad, factura_id, producto_id) VALUES (3, 2, 6);
